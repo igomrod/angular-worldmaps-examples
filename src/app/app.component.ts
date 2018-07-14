736ff2ab64dc3e7257a11649de51c1b7cb79b9eb
Iván Gómez <i.gomez.rdguez@gmail.com>
2018-07-14 11:07:28 +0200

    added example worldmap with google charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  geoChartData =  {
+    chartType: 'GeoChart',
+    dataTable: [
+      ['Country', 'Num of shared'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 500],
+      ['France', 600],
+      ['RU', 700]
+    ],
+    options: {'title': 'Wordmap'},
+  };
 }
